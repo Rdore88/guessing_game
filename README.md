bd8b97d003eeb82f60b7df98c4b7b22c5c35dc7f
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-02-14 15:50:19 -0500

    Program Readme and adding an array to make fun of the user if they put in a number more than once.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Read me for numbers guessing game project
+This program is a number guessing game were the computer will pick a number
+ from 1-100 and give you hints to help you guess it correctly but you only have
+ 5 tries before you lose. If you pick the same number twice it will make fun of
+ you!
--- guessing_game.rb
+++ guessing_game.rb
@@ -1,20 +1,26 @@
-def higher(number, guessed)
-  puts "You guessed #{number} which is too high"
-  if guessed == 5
+numbers_guessed = []
+
+def higher(number, guessed, value)
+    if guessed == 5
     puts "Too many guesses you lose, better luck next time"
     return false
-  elsif guessed < 5
+  elsif value == true
+    puts "You guessed #{number} again which is too high still, are you ok?"
     puts "Please guess again."
+  else
+    puts "You guessed #{number} which is too high."
   end
 end
 
-def lower(number, guessed)
-  puts "You guessed #{number} which is too low"
-  if guessed == 5
-    puts "Sorry you lose, better luck next time"
+def lower(number, guessed, value)
+    if guessed == 5
+    puts "Too many guesses you lose, better luck next time"
     return false
-  elsif guessed < 5
+  elsif value == true
+    puts "You guessed #{number} again which is too low still, are you ok?"
     puts "Please guess again."
+  else
+    puts "You guessed #{number} which is too low."
   end
 end
 
@@ -25,10 +31,20 @@ while questions_asked < 5
 user_guess = gets.chomp.to_i
   if user_guess < answer
       questions_asked += 1
-      lower(user_guess, questions_asked)
+      if numbers_guessed.include?(user_guess)
+        lower(user_guess, questions_asked, numbers_guessed.include?(user_guess))
+      else
+        lower(user_guess, questions_asked, numbers_guessed.include?(user_guess))
+        numbers_guessed.push(user_guess)
+      end
   elsif user_guess > answer
     questions_asked += 1
-    higher(user_guess, questions_asked)
+      if numbers_guessed.include?(user_guess)
+        higher(user_guess, questions_asked, numbers_guessed.include?(user_guess))
+      else
+        higher(user_guess, questions_asked, numbers_guessed.include?(user_guess))
+        numbers_guessed.push(user_guess)
+      end
   else
     puts "Congrats you guessed the right number!"
     break
